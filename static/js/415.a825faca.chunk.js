e020540bc3859cb15035829ca69a055c1976acba
engelisart <144841027+engelisart@users.noreply.github.com>
2023-12-26 23:03:57 +0000

    Deploying to gh-pages from @ engelisart/goit-react-hw-08-phonebook@7dbace21501bf473af0cfe0ec1a5c263fb4ca5f8 🚀

---
+++ static/js/415.a825faca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h1",{children:"Welcome"})})}}}]);
+//# sourceMappingURL=415.a825faca.chunk.js.map
